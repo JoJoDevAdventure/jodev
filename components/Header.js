d54e70f13b4fb4a9f0d05986cc95d24d9af2e323
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-10-26 22:50:40 +0100

    Header init

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
 
 
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">header</div>
+    </header>
+  );
 };
 
 export default Header;
