d6a407e669a4cf02017e918db295214f359b8c0d
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-10-27 11:03:03 +0100

    Header Done

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,23 @@
+import Link from "next/link";
+
+import { RiGithubLine, RiLinkedinLine, RiTwitterLine } from 'react-icons/ri';
+
+
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <div className="flex items-center gap-x-5 text-xl">
+    <Link href={''} className="hover:text-accent transition0all duration-300">
+      <RiLinkedinLine/>
+    </Link>
+    <Link href={''} className="hover:text-accent transition0all duration-300">
+      <RiGithubLine/>
+    </Link>
+    <Link href={''} className="hover:text-accent transition0all duration-300">
+      <RiTwitterLine/>
+    </Link>
+  </div>
+  )
 };
 
 export default Socials;
