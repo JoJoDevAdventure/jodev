e1f4abd4a34f403500ed87495893367e7d8e7784
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-11-06 15:27:30 +0100

    + Services Title

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,36 @@
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import ServicesSlider from "../../components/ServiceSlider";
+
+//framer motion
+import { } from "../../variants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[300vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">
+              {" "}
+              My services <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            Anim eu cupidatat reprehenderit do deserunt aute non ad sint enim.
+            Excepteur reprehenderit ea ex ut adipisicing nisi irure incididunt
+            exercitation nisi eiusmod exercitation nulla et. Ullamco elit non
+            quis et minim esse.
+          </p>
+          </div>
+          {/* Slider */}
+          <ServicesSlider/>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
