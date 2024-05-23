c37fcbe0e61d3fa78129bf586e29ad1a7732fe9d
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-05-23 12:55:04 +0100

    update

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,15 @@ import Socials from '../components/Socials';
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <header className="absolute z-30 w-full flex items-center px-2 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <div className="flex flex-row items-center gap-y-4 py-0 lg:py-8 justify-between">
           {/* LOGO */}
           <Link href="/">
             <Image
             src={'/logo.png'}
-            width={220}
-            height={48}
+            width={150}
+            height={50}
             alt=""
             priority={true}
             />
